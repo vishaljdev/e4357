15ca47a0e98cf218fbb31d97803e70f68cf611d6
dummy <dummy@server.fake>
2013-11-04 17:11:30 -0800

    hw1/.gitconfig

--- hw1/hw1_lib.c
+++ hw1/hw1_lib.c
@@ -8,13 +8,39 @@
 
 uint16_t gray_to_binary(uint16_t gray)
 {
-   /* add your code here */
-   return gray;
+	uint16_t get_val= 0b1000000000000000;
+	uint16_t val1, val2, bin_val, temp;
+        unsigned char i;
+
+        bin_val = gray & get_val;
+        val1 = bin_val;
+        for(i=1; i<16; i++) {
+                val1>>=1;
+                val2 = gray & (get_val >> 1);
+                bin_val|=(val1 ^ val2);
+                get_val>>=1;
+                val1 = (bin_val & get_val);
+        }
+	gray = bin_val;
+	return gray;
 }
+
 uint16_t binary_to_gray(uint16_t binary)
 {
-   /* add your code here */
-   return binary;
+	uint16_t get_val= 0b1000000000000000;
+        uint16_t val1, val2, gray_val, temp;
+        unsigned char i;
+
+        gray_val = binary & get_val;
+        for(i=1; i<16; i++) {
+                val1 = binary & get_val;
+                val1>>=1;
+                val2 = binary & (get_val >> 1);
+                gray_val|=(val1 ^ val2);
+                get_val>>=1;
+        } 
+	binary = gray_val;
+	return binary;
 }
 
 
